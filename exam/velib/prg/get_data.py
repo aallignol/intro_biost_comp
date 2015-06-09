376bdc2ef930f3387f98937ff1a22fbeaabd23a7
Arthur Allignol <arthur.allignol@uni-ulm.de>
2015-06-09 16:14:13 +0200

    velib project
    script for getting the data

---
+++ exam/velib/prg/get_data.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import json
+import urllib2
+import sys
+import argparse
+import numpy as np
+import pandas as pd
+import datetime
+
+parser = argparse.ArgumentParser(description="Get velib data")
+parser.add_argument('-a', '--apikey',
+                    help = 'api-key for accessing JCDecaux dev page',
+                    required = True)
+parser.add_argument('-c', '--contract',
+                    help = 'From which contract to get data',
+                    default = 'Paris')
+args = parser.parse_args()
+contract = args.contract
+print(contract)
+api_key = args.apikey
+
+my_url = "https://api.jcdecaux.com/vls/v1/stations?contract=" + contract + "&apiKey=" +api_key
+
+r = urllib2.urlopen(my_url)
+
+in_data = json.load(r)
+
+# Get the actual date
+actual_time = datetime.datetime.now()
+
+# transform the dict position into 'variables'
+for i in in_data:
+    i[u'latitude'] = i['position']['lat']
+    i[u'longitude'] = i['position']['lng']
+    del i['position']
+    del i['last_update']
+    i[u'last_update'] = actual_time.strftime('%Y/%m/%d %H:%M:%S')
+
+velib_data = pd.DataFrame.from_dict(in_data)
+
+data_name = "../data/velib_data_" + actual_time.strftime('%Y_%m_%d_%H') + ".csv"
+
+velib_data.to_csv(data_name, encoding = 'utf-8')
---
+++ exam/velib/project_description.md
@@ -0,0 +1,39 @@
+---
+title: One week of Vélib' in Paris
+css: GeneralStyle.css
+
+---
+
+# Introduction
+
+Vélib' is a large-scale public bike sharing system operated by
+[JCDecaux](http://www.jcdecaux.com/en/) in Paris and close suburbs. It
+comprises around 18000 bicycles distributed over 1230 bicycle
+stations.
+
+# Data description
+
+You will be provided with real-time data on each station that provide
+information on the station itself
+
+- Address
+- Latitude and longitude
+
+as well as updated information on
+
+- whether the station is open
+- number of available bikes
+- number of available bike stands
+- number of bike stands that are operational
+
+The data will be updated every hour and cover a period of one week.
+
+# Possible research question
+
+1. Study the flow of bicycle availability during a week day. Is that
+   true that there is a inflow of bicycles from the outskirts to
+   the city centre earlier in the day, and a net flow outwards in the
+   evening
+2. is there a difference in the flow of bicycles between week days and
+   weekends (day/night)
+3. ...
